5a9db2427ef639a197c5f465c36e2b814d8d5c84
hodorsag <hodorsag@gmail.com>
2020-06-23 15:10:47 +0300

    Change index.py again

--- index.py
+++ index.py
@@ -3,13 +3,13 @@ import time
 import collections
 import threading
 paths = list()
-directory = "D:\Coursework"
+directory = "A:\Coursework"
 
 
 def inverted_index(plist, index):
     for i in plist:
-        with open(i, "r") as f:
-            lexems = f.read().split(" ")
+        with open(i, "r") as f: # открываем первый файл только чтением как f (как переприсваиванием)
+            lexems = f.read().split(" ") # читаем строку до пробела и впихиваем её в лексемы
             for lexem in lexems:
                 for letter in lexem:
                     if letter.isalpha() or letter == "'":
@@ -19,11 +19,11 @@ def inverted_index(plist, index):
                     index[lexem].append(i)
 
 def main():
-    threads = []
-    start = 0
+    threads = []  # список потоков
+    start = 0 # начало и конец отсчета времени
     end = 0
-    index = collections.defaultdict(list)
-    for d, dirs, files in os.walk(directory):
+    index = collections.defaultdict(list) # создаем словарь (ключ и значение) где слово ключ, а список путей - значение
+    for d, dirs, files in os.walk(directory): # вносим в один большой список все пути к файлам
         for f in files:
             if os.path.isdir(f):
                 continue
@@ -33,18 +33,18 @@ def main():
 
     thread_count = input("Input number of threads: ")
 
-    chunk = len(paths)//int(thread_count)
+    chunk = len(paths)//int(thread_count) # то, сколько должен будет взять каждый поток
 
     start = time.time()
 
-    count = int(thread_count)
+    count = int(thread_count)# разование кол-ва потоков из строки в инт
 
     for id in range(count):
          process = threading.Thread(target = inverted_index, args = (paths[id*chunk: id*chunk + chunk], index))
-         process.start()
-         threads.append(process)
+         process.start() # начинаем поток
+         threads.append(process) #запихиваем поток в список потоков
 
-    for i in threads:
+    for i in threads:# ждем завершение свех потоков
         i.join()
 
     end = time.time()
